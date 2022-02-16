3a16d292cc972e816a3d0fc52d205ba0c994b987
bibimagic <b23.martial@gmail.com>
2022-02-16 09:21:32 +0100

    Add files via upload

---
+++ Aquila-WakeOnLAN.2.12.4/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿
+$packageArgs = @{
+  packageName   	= 'Aquila-WakeOnLAN'
+  fileType      	= 'exe'
+  url           	= "https://github.com/basildane/WakeOnLAN/releases/download/2.12.4/WakeOnLAN_2.12.4.0.exe"
+  softwareName  	= 'Aquila-WakeOnLAN'
+  checksum      	= 'BD5E0E07B49BEAE5CBFDB8AA74596ED9E65B7A4BC68E2C6176C8E06ABB232299'
+  checksumType  	= 'sha256'
+  silentArgs 		= '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCLOSEAPPLICATIONS /NORESTARTAPPLICATIONS'
+  validExitCodes 	= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
